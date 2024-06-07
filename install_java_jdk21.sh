b36d536b7b2e75442b4453fc6a7bb949d22b5e1b
Victor Gauthereau <vgauther@student.42.fr>
2024-06-07 21:57:51 +0200

    Update install_java_jdk21.sh

--- install_java_jdk21.sh
+++ install_java_jdk21.sh
@@ -2,4 +2,7 @@ wget https://download.oracle.com/java/21/archive/jdk-21.0.2_linux-x64_bin.deb
 
 sudo dpkg -i jdk-21.0.2_linux-x64_bin.deb
 sudo apt-get install -f
+
+rm -rf jdk-21.0.2_linux-x64_bin.deb
+
 java -version
